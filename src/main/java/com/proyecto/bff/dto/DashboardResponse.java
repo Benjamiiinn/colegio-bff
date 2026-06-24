dbcf6d9deb8435f7455af947a50b1bb20380db90
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2026-06-23 22:14:26 -0400

    chore: remove unrelated files from controller branch

--- src/main/java/com/proyecto/bff/dto/DashboardResponse.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.bff.dto;
-
-import java.util.Map;
-
-public record DashboardResponse(
-        String rol,
-        Map<String, Object> data
-) {
-}
--- src/main/java/com/proyecto/bff/exception/BffException.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.bff.exception;
-
-public class BffException extends RuntimeException {
-    public BffException(String message) {
-        super(message);
-    }
-}
